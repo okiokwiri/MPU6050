e73107935cb2c7ff3b058fc5216dd52aedf805b5
Oki Okwiri <111518495+okiokwiri@users.noreply.github.com>
2024-01-09 18:05:00 +0300

    Removed arduino and HW related includes from MPU6050.h

--- src/ElectronicCatsDriver/MPU6050.h
+++ src/ElectronicCatsDriver/MPU6050.h
@@ -38,23 +38,7 @@ THE SOFTWARE.
 #ifndef _MPU6050_H_
 #define _MPU6050_H_
 
-#include "I2Cdev.h"
-#include "helper_3dmath.h"
 
-// supporting link:  http://forum.arduino.cc/index.php?&topic=143444.msg1079517#msg1079517
-// also: http://forum.arduino.cc/index.php?&topic=141571.msg1062899#msg1062899s
-
-#ifdef __AVR__
-#include <avr/pgmspace.h>
-#elif defined(ESP32)
-    #include <pgmspace.h>
-#else
-//#define PROGMEM /* empty */
-//#define pgm_read_byte(x) (*(x))
-//#define pgm_read_word(x) (*(x))
-//#define pgm_read_float(x) (*(x))
-//#define PSTR(STR) STR
-#endif
 
 
 #define MPU6050_ADDRESS_AD0_LOW     0x68 // address pin low (GND), default for InvenSense evaluation board
